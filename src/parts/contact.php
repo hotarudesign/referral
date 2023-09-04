ba9c72bd9645022c1cc930b74a8f42016637df1e
hotarudesign <hotarudesign1220@gmail.com>
2023-09-04 15:10:14 +0900

    💄 会社住所の反映

--- src/parts/contact.php
+++ src/parts/contact.php
@@ -3,7 +3,7 @@
         <h2 class="contact-ttl sec-ttl">Contact<span class="contact-ttl-ruby sec-ttl-ruby">応募方法</span></h2>
         <div class="contact__body">
             <p class="contact__body-desc">ご不明な点がございましたら<br class="sp-only">ぜひお気軽にお問い合わせください。</p>
-            <p class="contact__body-detail">〒000-0000　東京都住所が入ります</p>
+            <p class="contact__body-detail">〒108-0075　東京都港区港南1-2-70　品川シーズンテラス18階</p>
             <p class="contact__body-detail">カシワバラグループ　採用担当 松崎</p>
             <p class="contact__body-detail">
                 <a href="tel:080-0000-0000">TEL:080-0000-0000</a>
