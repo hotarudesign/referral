fe92e5ad3024766f3ff8955274ba7b8a041bf6da
hotarudesign <hotarudesign1220@gmail.com>
2023-09-20 16:37:12 +0900

    💄 画像の整理とFix _link削除

--- src/parts/home/fix-link.php
+++
@@ -1,9 +0,0 @@
-<div class="fix-link pc">
-    <a href="https://thkd5375.talent-p.net/EntryFormMng/Entryform?url_token=b71d970ffcf54e6c9b2478287e7757e1" class="fix-link__btn">
-        <p class="fix-link__btn-txt">エントリーはこちら</p>
-        <picture class="fix-link__btn-icon">
-            <source srcset='<?php echo ImgPath; ?>/webp/check-icon.webp' type='image/webp'>
-            <img src='<?php echo ImgPath; ?>/check-icon.png' alt=''>
-        </picture>
-    </a>
-</div>
